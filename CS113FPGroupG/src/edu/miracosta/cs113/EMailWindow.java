fff7ccbfc868f75e30834ba0741cef73eb9943f7
Ryo Kanda <rensakou.touhou@gmail.com>
2017-05-12 12:19:27 -0700

    Group G GUI Update Commit
    Beautiful GUI update

--- CS113FPGroupG/src/edu/miracosta/cs113/EMailWindow.java
+++ CS113FPGroupG/src/edu/miracosta/cs113/EMailWindow.java
@@ -3,9 +3,15 @@ package edu.miracosta.cs113;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 import javax.swing.JTextField;
+import javax.swing.JTextArea;
+import javax.swing.JScrollPane;
 import javax.swing.JButton;
 import javax.swing.JLabel;
 import java.awt.BorderLayout;
+import javax.swing.SpringLayout;
+import javax.swing.SwingConstants;
+import javax.swing.border.EtchedBorder;
+
 import java.awt.Dimension;
 
 @SuppressWarnings("serial")
@@ -14,15 +20,18 @@ public class EMailWindow extends JFrame{
 	JPanel mainPanel;
 	
 	JPanel infoPanel;
+	
 	JLabel senderLabel;
 	JLabel subjectLabel;
 	JLabel dateLabel;
+	
 	JTextField senderField;
 	JTextField subjectField;
 	JTextField dateField;
 	
 	JPanel descriptionPanel;
-	JTextField inputField;
+	JTextArea inputArea;
+	JScrollPane inputScroll;
 
 	JPanel buttonPanel;
 	JButton open;
@@ -31,20 +40,59 @@ public class EMailWindow extends JFrame{
 	JButton save;
 	
 	public EMailWindow(){
-		
+		// Default window setup
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		setMinimumSize(new Dimension(400, 300));
+		setMinimumSize(new Dimension(200,250));
 		
 		mainPanel = new JPanel(new BorderLayout());
 		add(mainPanel);
 		
-		infoPanel = new JPanel();
+		// InfoPanel setup
+		infoPanel = new JPanel(new SpringLayout());
+		senderLabel = new JLabel("Sender:", SwingConstants.RIGHT);
+		infoPanel.add(senderLabel, BorderLayout.WEST);
+
+		senderField = new JTextField();
+		infoPanel.add(senderField, BorderLayout.CENTER);
+		
+		subjectLabel = new JLabel("Subject:", SwingConstants.RIGHT);
+		infoPanel.add(subjectLabel, BorderLayout.WEST);
+
+		subjectField = new JTextField();
+		infoPanel.add(subjectField, BorderLayout.CENTER);
+		
+		dateLabel = new JLabel("Date:", SwingConstants.RIGHT);
+		infoPanel.add(dateLabel, BorderLayout.WEST);
+		
+		dateField = new JTextField();
+		infoPanel.add(dateField, BorderLayout.CENTER);
+
+		SpringUtilities.makeCompactGrid(infoPanel, 3, 2, 5, 5, 5, 5);
+		
 		mainPanel.add(infoPanel, BorderLayout.NORTH);
 		
+		// DescriptionPanel setup
 		descriptionPanel = new JPanel(new BorderLayout());
+		inputArea = new JTextArea();
+		inputScroll = new JScrollPane(inputArea);
+		inputArea.setLineWrap(true);
+		EtchedBorder border = new EtchedBorder(EtchedBorder.RAISED);
+		inputArea.setBorder(border);
+		descriptionPanel.add(inputScroll, BorderLayout.CENTER);
 		mainPanel.add(descriptionPanel, BorderLayout.CENTER);
 		
-		buttonPanel = new JPanel();
+		// ButtonPanel setup
+		buttonPanel = new JPanel(new SpringLayout());
+		open = new JButton("Open");
+		encrypt = new JButton("Encrypt");
+		decrypt = new JButton("Decrypt");
+		save = new JButton("Save");
+		buttonPanel.add(open);
+		buttonPanel.add(encrypt);
+		buttonPanel.add(decrypt);
+		buttonPanel.add(save);
+		SpringUtilities.makeGrid(buttonPanel, 4, 1, 5, 5, 5, 5);
+		
 		descriptionPanel.add(buttonPanel, BorderLayout.EAST);
 
 		setVisible(true);
